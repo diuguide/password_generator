9a7150a4dd124abe744403558eae99982dd512ba
diuguide <61917141+diuguide@users.noreply.github.com>
2020-06-01 18:46:08 -0400

    added else if statments to cover possible passwords

--- script.js
+++ script.js
@@ -6,19 +6,12 @@ const charSetSpecial = ["!@#$%^&*?"];
 const charSetNumbers = ["0123456789"];
 
 generateBtn.addEventListener('click', function writePassword() {
-    const passLength = prompt('Enter a number for the length of your password between 8-128');
-    const passUpper = confirm('Include Upper Case letters?');
-    const passLower = confirm('Include Lower Case Letters?');
-    const passSpecial = confirm('Include Special Characters?');
-    const passNumber = confirm('Include Numbers?');
-    const passConfirm = confirm('Length:' + passLength + ' Uppercase: ' + passUpper + ' Lowercase: ' + passLower + ' Special Characters: ' + passSpecial + ' Numbers: ' + passNumber +'?');
-
-    console.log(passLength);
-    console.log(passUpper);
-    console.log(passLower);
-    console.log(passSpecial);
-    console.log(passNumber);
-    console.log(passConfirm); 
+    const passLength = prompt('Length of Password:');
+    const passUpper = confirm('Do you want to include uppercase letters?');
+    const passLower = confirm('Do you want to include lowercase letters?');
+    const passSpecial = confirm('Do you want to include !@#$%^&*?');
+    const passNumber = confirm('Do you want to include Numbers?');
+    const passConfirm = confirm('PLEASE REVIEW :: Your password will have ' + passLength + ' characters ' + '|| Include uppercase: ' + passUpper + ' || Include lowercase: ' + passLower + ' || Include special characters: ' + passSpecial + ' || Include numbers: ' + passNumber);
 
     if (passUpper === true && passLower === true && passSpecial === true && passNumber === true) {
       function generatePassword (passLength) {
@@ -74,6 +67,16 @@ generateBtn.addEventListener('click', function writePassword() {
       function generatePassword (passLength) {
         let password = '';
         let charSet = charSetSpecial + charSetlettersUpper;
+        
+        for (let i=0; i < passLength; i++ ) {
+        password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+        }  
+        return password;
+      } 
+    } else if (passUpper === true && passLower === false && passSpecial === false && passNumber === true) {
+      function generatePassword (passLength) {
+        let password = '';
+        let charSet = charSetNumbers + charSetlettersUpper;
         
         for (let i=0; i < passLength; i++ ) {
         password += charSet.charAt(Math.floor(Math.random() * charSet.length));
